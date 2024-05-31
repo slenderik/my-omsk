5b08a91d0052d4d13140f384109f4d0f57aa6186
slenderik <slenderik2@gmail.com>
2024-05-31 19:40:14 +0600

    refactor!: change simple web components to blade components

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -16,32 +16,57 @@
         </div>
     @endif
 
-    <form action="{{ route('events.new') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('events.new') }}" method="POST" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
+
         <div>
-            <label for="english_name">Англ. название:</label>
-            <input type="text" id="english_name" name="english_name" value="{{ old('english_name') }}">
+            <x-input-label for="english_name" :value="__('Англ. название')" />
+            <x-text-input id="english_name" name="english_name" type="text" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('english_name')" class="mt-2" />
         </div>
+
         <div>
-            <label for="title">Заголовок:</label>
-            <input type="text" id="title" name="title" value="{{ old('title') }}">
+            <x-input-label for="title" :value="__('Заголовок')" />
+            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('title')" class="mt-2" />
         </div>
+        
         <div>
-            <label for="background_colour">Background colour:</label>
-            <input type="text" id="background_colour" name="background_colour" value="{{ old('background_colour') }}">
+            <x-input-label for="background_colour" :value="__('Фоновый цвет')" />
+            <x-text-input id="background_colour" name="background_colour" type="color" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('background_colour')" class="mt-2" />
         </div>
+
         <div>
-            <label for="description">Описание:</label>
-            <textarea id="description" name="description">{{ old('description') }}</textarea>
+            <x-input-label for="description" :value="__('Описание')" />
+            <x-text-input id="description" name="description" type="text" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('description')" class="mt-2" />
         </div>
+
         <div>
-            <label for="image">Картинка:</label>
-            <input type="file" id="image" name="image">
+            <x-input-label for="image" :value="__('Картинка')" />
+            <x-text-input id="image" name="image" type="file" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('image')" class="mt-2" />
         </div>
+
         <div>
-            <label for="image_alt">Опишите картинку:</label>
-            <input type="text" id="image_alt" name="image_alt">
+            <x-input-label for="image_alt" :value="__('Описание картинки')" />
+            <x-text-input id="image_alt" name="image_alt" type="text" class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('image_alt')" class="mt-2" />
         </div>
-        <button type="submit">Создать</button>
+
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Добавить ивент') }}</x-primary-button>
+
+            @if (session('status') === 'events-new')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600 dark:text-gray-400"
+                >{{ __('Ивент добавлен.') }}</p>
+            @endif
+        </div>   
     </form>
 </x-app-layout>
