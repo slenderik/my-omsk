91d2d155772b4bbb84e2ea94f18bab4e69aa7ae2
slenderik <slenderik2@gmail.com>
2024-05-31 21:11:58 +0600

    style: add css to event card

--- resources/views/events/card.blade.php
+++ resources/views/events/card.blade.php
@@ -7,14 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        
-                <tr>
-                    <td>{{ $event->id }}</td>
-                    <td>{{ $event->title }}</td>
-                    <td>{{ $event->descripion }}</td>
-                    <td>{{ $event->background_colour }}</td>
-                </tr>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">    
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @if ($event->eventImage)
+                        <img
+                            class="event-page__img"
+                            alt="{{ $event->eventImage->image_alt }}"
+                            src="{{ asset('storage/images/'.$event->eventImage->image_name) }}"
+                        >
+                    @endif
+                
+                    <h1 class="event-page__title">{{ $event->title }}</h1>
+                    <p class="event-page__description">{{ $event->description }}</p>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,68 +5,65 @@
         </h2>
     </x-slot>
 
-    <!-- Вывод ошибок валидации -->
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                <form action="{{ route('events.new') }}" method="POST" class="mt-6 space-y-6" enctype="multipart/form-data">
+                    @csrf
+
+                    <div>
+                        <x-input-label for="english_name" :value="__('Англ. название')" />
+                        <x-text-input id="english_name" name="english_name" type="text" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('english_name')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="title" :value="__('Заголовок')" />
+                        <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+                    
+                    <div>
+                        <x-input-label for="background_colour" :value="__('Фоновый цвет')" />
+                        <x-text-input id="background_colour" name="background_colour" type="color" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('background_colour')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="description" :value="__('Описание')" />
+                        <x-text-input id="description" name="description" type="text" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="image" :value="__('Картинка')" />
+                        <x-text-input id="image" name="image" type="file" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="image_alt" :value="__('Описание картинки')" />
+                        <x-text-input id="image_alt" name="image_alt" type="text" class="mt-1 block w-full" />
+                        <x-input-error :messages="$errors->get('image_alt')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <x-primary-button>{{ __('Добавить ивент') }}</x-primary-button>
+
+                        @if (session('status') === 'events-new')
+                            <p
+                                x-data="{ show: true }"
+                                x-show="show"
+                                x-transition
+                                x-init="setTimeout(() => show = false, 2000)"
+                                class="text-sm text-gray-600 dark:text-gray-400"
+                            >{{ __('Ивент добавлен.') }}</p>
+                        @endif
+                    </div>   
+                </form>
+                
+            </div>
         </div>
-    @endif
-
-    <form action="{{ route('events.new') }}" method="POST" class="mt-6 space-y-6" enctype="multipart/form-data">
-        @csrf
-
-        <div>
-            <x-input-label for="english_name" :value="__('Англ. название')" />
-            <x-text-input id="english_name" name="english_name" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('english_name')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="title" :value="__('Заголовок')" />
-            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('title')" class="mt-2" />
-        </div>
-        
-        <div>
-            <x-input-label for="background_colour" :value="__('Фоновый цвет')" />
-            <x-text-input id="background_colour" name="background_colour" type="color" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('background_colour')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="description" :value="__('Описание')" />
-            <x-text-input id="description" name="description" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('description')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="image" :value="__('Картинка')" />
-            <x-text-input id="image" name="image" type="file" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('image')" class="mt-2" />
-        </div>
-
-        <div>
-            <x-input-label for="image_alt" :value="__('Описание картинки')" />
-            <x-text-input id="image_alt" name="image_alt" type="text" class="mt-1 block w-full" />
-            <x-input-error :messages="$errors->get('image_alt')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Добавить ивент') }}</x-primary-button>
-
-            @if (session('status') === 'events-new')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Ивент добавлен.') }}</p>
-            @endif
-        </div>   
-    </form>
+    </div>
 </x-app-layout>
