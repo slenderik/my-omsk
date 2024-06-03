2ff2437111742b1733859d042becd631c7702cea
slenderik <slenderik2@gmail.com>
2024-06-04 00:10:13 +0600

    refactor: create a component for org card

---
+++ resources/views/components/organizator-card.blade.php
@@ -0,0 +1,8 @@
+{{-- organization card --}}
+<a href="/event/{{ $organization->id }}">
+    <div class="organizator-card">
+        <img src="" alt="">
+        <p>{{ $organization->name }}</p>
+        <p>{{ $organization->description }}</p>
+    </div>
+</a>
