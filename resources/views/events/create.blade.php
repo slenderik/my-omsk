d3de44ee81b43e63b88e9937aac05269c5480449
slenderik <slenderik2@gmail.com>
2024-06-03 12:47:56 +0600

    refactor: rename a events.index

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Добавить ивент') }}
+            {{ __('Создать ивент') }}
         </h2>
     </x-slot>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,7 +25,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('events.create')" :active="request()->routeIs('events.create')">
-                        {{ __('Добавить ивент') }}
+                        {{ __('Создать ивент') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
