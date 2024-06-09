575ac1b5e1357505120a02dcd9d139ef0ec88fa1
slenderik <slenderik2@gmail.com>
2024-06-10 01:02:42 +0600

    fix: nullable image

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -3,18 +3,15 @@
 {{-- event card --}}
 <a href="/event/{{ $event->id }}">
     <div class="event-card" style="background-color: {{ $event->background_colour }}">
-        @if ($event->eventImage)
-            <img
-                class="event-card__img"
+        @if ($event?->eventImage)
+            <img class="event-card__img"
                 alt="{{ $event->eventImage->image_alt }}"
-                src="{{ asset('storage/images/'.$event->eventImage->image_name) }}"
-            >
+                src="{{ asset('storage/images/'.$event->eventImage->image_name) }}">
         @else
             <img src="{{ asset('default-image-path') }}" alt="Default Image" class="event-card__img">
         @endif
         <div class="event-card__bottom-part">
             <p>{{ $event->title }}</p>
         </div>
-        <svg></svg>
     </div>
 </a>
